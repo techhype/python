dc080bdfa63eee933280c2cd61259501ede28e16
unknown <krizzkishore@gmail.com>
2017-12-01 18:25:15 +0530

    Python operations +-*/

---
+++ addnum.py
@@ -0,0 +1,24 @@
+
+
+def main():
+    a=int(input('a:'))
+    b=int(input('b:'))
+#addition
+    print("a + b = ",a+b)
+
+#subraction
+    print("a - b = ",a-b)
+
+#multiplication
+    print("a * b = ",a*b)
+
+#for floating point division
+    print("a / b = ",a/b)
+
+#for integer(floor) division
+    print("a // b = ",a//b)
+
+
+
+if __name__ == '__main__':
+    main()
